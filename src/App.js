42e80b07ffbb056e0107fb376379c01e134a6e11
Ozan Tekin <ardilozantekin@gmail.com>
2022-12-11 23:51:43 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import './index.css'
+
 export default function App() {
   return (
     <div>
-      <h1 className='text-red-500'>Selam!</h1>
+      <h1 className='text-red-500 app'>Selam!</h1>
     </div>
   )
 }
